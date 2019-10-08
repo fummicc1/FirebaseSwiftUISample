a4c65e878b084e990d687fcdb1acf103e0662cb7
fummicc1 <fumiya989@gmail.com>
2019-10-09 01:35:55 +0900

    Initial Commit

---
+++ FirebaseSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseSwiftUI
+//
+//  Created by Fumiya Tanaka on 2019/10/09.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
